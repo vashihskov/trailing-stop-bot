2fb57ab6334d93861e31c42f154b5ab1a042f10a
vashishkov <30894678+vashishkov@users.noreply.github.com>
2021-05-31 22:47:44 +0700

    Update bot.go

--- bot.go
+++ bot.go
@@ -13,14 +13,25 @@ import (
 )
 
 var token = flag.String("token", "", "my token")
+var production = true
+var endDayClosePosition = false
+var StopLossPerc = 0.5
+var StopLossCurrent float64 = 0.0
+var hours, minutes, _ = time.Now().Clock()
 
 func main() {
 	log.Println("Starting bot")
+	cleanState()
 	for {
 		rest()
 		time.Sleep(10 * time.Second)
 	}
 
+}
+
+func cleanState() {
+	os.RemoveAll("./state/")
+	os.MkdirAll("./state/", 0700)
 }
 
 func rest() {
@@ -35,68 +46,109 @@ func rest() {
 
 	for i := range positions {
 
-		var StopLossPerc = 0.5
-		var StopLossCurrent float64 = 0.0
-
 		orderbook, err := client.Orderbook(ctx, 1, positions[i].FIGI)
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		if positions[i].Lots > 0 {
+		// Close positions on end of the day
+		if positions[i].Ticker != string("USD000UTSTOM") && (positions[i].AveragePositionPrice.Currency == "RUB" && hours == 00 && minutes == 50) || (positions[i].AveragePositionPrice.Currency == "USD" && hours == 04 && minutes == 40) {
+			err := os.Remove("./state/short-" + positions[i].Ticker + ".json")
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if production == true && endDayClosePosition == true {
+				_, err := client.MarketOrder(ctx, sdk.DefaultAccount, positions[i].FIGI, positions[i].Lots, sdk.BUY)
+				if err != nil {
+					log.Fatalln(err)
+				}
+
+				ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				log.Println("Close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+			} else {
+				log.Println("Time to close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+			}
+
+		} else if positions[i].Lots > 0 && positions[i].Ticker != string("USD000UTSTOM") {
 			StopLossCalculated := orderbook.LastPrice - orderbook.LastPrice/100*StopLossPerc
 
-			if _, err := os.Stat(positions[i].FIGI); os.IsNotExist(err) {
+			if _, err := os.Stat("./state/long-" + positions[i].Ticker + ".json"); os.IsNotExist(err) {
 				file, _ := json.MarshalIndent(StopLossCalculated, "", "")
-				_ = ioutil.WriteFile(positions[i].FIGI, file, 0644)
+				_ = ioutil.WriteFile("./state/long-"+positions[i].Ticker+".json", file, 0644)
 			}
-			file, _ := ioutil.ReadFile(positions[i].FIGI)
+			file, _ := ioutil.ReadFile("./state/long-" + positions[i].Ticker + ".json")
 			_ = json.Unmarshal([]byte(file), &StopLossCurrent)
 
 			if StopLossCurrent <= orderbook.LastPrice {
 				if StopLossCurrent < StopLossCalculated {
 					StopLossCurrent := StopLossCalculated
 					file, _ := json.MarshalIndent(StopLossCurrent, "", "")
-					_ = ioutil.WriteFile(positions[i].FIGI, file, 0644)
+					_ = ioutil.WriteFile("./state/long-"+positions[i].Ticker+".json", file, 0644)
 
-					log.Println("Move stop-loss order forward for", orderbook.FIGI, orderbook.LastPrice, "/", StopLossCurrent, "/", positions[i].Lots)
+					log.Println("Move stop-loss order forward for", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
 				}
 
 			} else {
-				err := os.Remove(positions[i].FIGI)
+				err := os.Remove("./state/long-" + positions[i].Ticker + ".json")
 				if err != nil {
 					log.Fatalln(err)
 				}
+				if production == true {
+					_, err := client.MarketOrder(ctx, sdk.DefaultAccount, positions[i].FIGI, positions[i].Lots, sdk.SELL)
+					if err != nil {
+						log.Fatalln(err)
+					}
 
-				log.Println("Close position", orderbook.FIGI, orderbook.LastPrice, "/", StopLossCurrent, "/", positions[i].Lots)
+					ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+					defer cancel()
+
+					log.Println("Close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+				} else {
+					log.Println("Time to close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+				}
 			}
 
-		} else {
+		} else if positions[i].Lots < 0 && positions[i].Ticker != string("USD000UTSTOM") {
 			StopLossCalculated := orderbook.LastPrice + orderbook.LastPrice/100*StopLossPerc
 
-			if _, err := os.Stat(positions[i].FIGI); os.IsNotExist(err) {
+			if _, err := os.Stat("./state/short-" + positions[i].Ticker + ".json"); os.IsNotExist(err) {
 				file, _ := json.MarshalIndent(StopLossCalculated, "", "")
-				_ = ioutil.WriteFile(positions[i].FIGI, file, 0644)
+				_ = ioutil.WriteFile("./state/short-"+positions[i].Ticker+".json", file, 0644)
 			}
-			file, _ := ioutil.ReadFile(positions[i].FIGI)
+			file, _ := ioutil.ReadFile("./state/short-" + positions[i].Ticker + ".json")
 			_ = json.Unmarshal([]byte(file), &StopLossCurrent)
 
 			if StopLossCurrent > orderbook.LastPrice {
 				if StopLossCurrent > StopLossCalculated {
 					StopLossCurrent := StopLossCalculated
 					file, _ := json.MarshalIndent(StopLossCurrent, "", "")
-					_ = ioutil.WriteFile(positions[i].FIGI, file, 0644)
+					_ = ioutil.WriteFile("./state/short-"+positions[i].Ticker+".json", file, 0644)
 
-					log.Println("Move stop-loss order forward for", orderbook.FIGI, orderbook.LastPrice, "/", StopLossCurrent, "/", positions[i].Lots)
+					log.Println("Move stop-loss order forward for", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
 				}
 
 			} else {
-				err := os.Remove(positions[i].FIGI)
+				err := os.Remove("./state/short-" + positions[i].Ticker + ".json")
 				if err != nil {
 					log.Fatalln(err)
 				}
 
-				log.Println("Close position", orderbook.FIGI, orderbook.LastPrice, "/", StopLossCurrent, "/", positions[i].Lots)
+				if production == true {
+					_, err := client.MarketOrder(ctx, sdk.DefaultAccount, positions[i].FIGI, positions[i].Lots, sdk.BUY)
+					if err != nil {
+						log.Fatalln(err)
+					}
+
+					ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+					defer cancel()
+
+					log.Println("Close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+				} else {
+					log.Println("Time to close position", positions[i].Lots, positions[i].Ticker, orderbook.LastPrice, "/", StopLossCurrent)
+				}
 			}
 		}
 
